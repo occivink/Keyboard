d3e03d4fe08b299236503cf1ca531d4e8626cdbb
Olivier Perret <Olivier.Perret@mailbox.org>
2022-05-31 08:39:34 +0200

    firmware: actually debounce on the slave side
    And also change back the pins

--- firmware/src/main.c
+++ firmware/src/main.c
@@ -42,16 +42,16 @@ bool __no_inline_not_in_flash_func(get_bootsel_button)() {
 void set_led_on(bool on) { gpio_put(PICO_DEFAULT_LED_PIN, on); }
 
 // gpio_rows is defined left-to-right, and gpio_cols from top-to-bottom
-#define IS_LEFT 0
+#define IS_LEFT 1
 
 #if IS_LEFT
 
-const bool force_slave = true;
-const uint gpio_rows[] = {2, 6, 12, 14, 15};
-const uint gpio_cols[] = {11, 13, 7, 9, 4, 0};
+const bool force_slave = false;
+const uint gpio_rows[] = {2, 5, 8, 15, 10};
+const uint gpio_cols[] = {9, 14, 6, 7, 3, 28};
 #define UART uart0
-#define UART_TX 16
-#define UART_RX 17
+#define UART_TX 12
+#define UART_RX 13
 #define LEFT_KEY_TABLE this_key_table
 #define RIGHT_KEY_TABLE other_key_table
 
@@ -153,7 +153,7 @@ int main(void) {
     irq_set_enabled(UART0_IRQ, true);
     uart_set_irq_enables(UART, true, false);
 
-    const uint8_t debounce_cycles = 3;
+    const uint8_t debounce_cycles = 5;
     // the debounce table only applies to the current controller
     // the other half takes care of its own debouncing
     uint8_t debounce_table[5][6];
@@ -248,6 +248,7 @@ int main(void) {
                     } else {
                         bool set = gpio_get(gpio_cols[col]);
                         if (set != state_table[row][col]) {
+                            debounce_table[row][col] = debounce_cycles;
                             state_table[row][col] = set;
                             uint8_t value = count;
                             if (set)
